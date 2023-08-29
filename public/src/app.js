519a03ed66f5fad5340347d4b09768ead7ee7bbc
Tom-on64 <kubecekt@gmail.com>
2023-08-29 18:16:02 +0200

    A lot changed i guess?

--- public/src/app.js
+++ public/src/app.js
@@ -1,16 +1,33 @@
-let isDark = false;
+let theme = localStorage.getItem("theme");
+if (!theme) theme = "light";
+
+if (theme == "dark") {
+  document.body.classList.remove("light");
+  document.body.classList.add("dark");
+}
 
 const switchTheme = () => {
   const body = document.body;
-  if (isDark) {
+  if (theme == "dark") {
     body.classList.remove("dark");
     body.classList.add("light");
-    isDark = false;
+    theme = "light";
   } else {
     body.classList.remove("light");
     body.classList.add("dark");
-    isDark = true;
+    theme = "dark";
   }
+  localStorage.setItem("theme", theme);
 };
 
+document.getElementById("aboutBtn").onclick = () =>
+  (window.location.href = "/");
+document.getElementById("projectBtn").onclick = () =>
+  (window.location.href = "/project");
+document.getElementById("logsBtn").onclick = () =>
+  (window.location.href = "/logs");
+document.getElementById("contactBtn").onclick = () =>
+  (window.location.href = "/contact");
+
 document.getElementById("themeSwitcher").onclick = switchTheme;
+start();
